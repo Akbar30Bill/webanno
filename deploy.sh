ca934d1b342e77553cf8209d4f7d35f6882e0154
Ali Shahverdi <akbar30bill@Alis-MacBook-Pro.local>
2019-06-28 23:26:04 +0430

    init

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if[$whoami -eq 'root']
+then
+  mkdir /srv/webanno
+  echo installing Docker
+  apt install docker -y
+  apt install docker.io -y
+  echo Port to use:
+  read Port
+  echo Run Webanno in background?[Y/n]:
+  read background
+  if[$background -eq 'y'] || [$background -eq 'Y']
+  then
+    docker run -d --name webanno -v /srv/webanno:/export -p$Port:$Port webanno/webanno:3.5.7
+  else
+    docker run -it --name webanno -v /srv/webanno:/export -p$Port:$Port webanno/webanno:3.5.7
+  fi
+  echo creating systemd service
+  cp webanno.service /etc/systemd/system/webanno.service
+  systemctl enable webanno
+  echo Done!
+else
+  echo Please run with Superuser privilage
+fi
